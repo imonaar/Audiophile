99135d1aabc4e897ff858440b9aa69fb1cd6460a
imonaar <kevinmonari22@gmail.com>
2023-11-17 15:19:03 +0300

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,14 @@
 import type { Config } from 'tailwindcss'
+const { nextui } = require("@nextui-org/react");
+
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+
   ],
   theme: {
     extend: {
@@ -14,7 +18,20 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      // surface and text
+      'surface-1': "#FFFFFF",
+      'text-1': "#FFFFFF",
+
+      'surface-2': "#F1F1F1",
+      'text-2':"#000000",
+
+      // button
+      'bg-button':"#D87D4A",
+      'bg-button-hover':"#fbaf85",
+    }
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 }
 export default config
